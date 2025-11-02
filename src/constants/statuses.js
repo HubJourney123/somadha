405e138c4470b24483c991c657b3ba9021745693
Utsar Services <utsar.services@gmail.com>
2025-11-02 12:40:58 +0600

    Alhamdulillah

--- src/constants/statuses.js
+++ src/constants/statuses.js
@@ -16,7 +16,7 @@ export const STATUSES = [
     id: 3, 
     name: 'সমস্যাটি সমাধানের জন্য দেয়া হয়েছে', 
     nameEn: 'Assigned',
-    color: 'bg-amber-200'
+    color: 'bg-amber-800'
   },
   { 
     id: 4, 
